79f9ca940afd383f1294db40b99a09c793e901ee
garod <garod.ajemyan@thecoderiders.com>
2023-11-14 11:35:14 +0400

    cleared the useless stuff and initialized the project

---
+++ src/components/Menu.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../assets/styles/Menu.module.css'
+
+const Menu = () => {
+  return (
+    <div className={styles.menuContainer}>
+      Hi
+    </div>
+  )
+}
+
+export default Menu
